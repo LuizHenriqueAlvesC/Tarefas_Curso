86c24bdc1caa1b7da1d5387ca02bd353f583c533
luiz980alves@gmail.com <luiz980alves@gmail.com>
2024-02-12 20:08:17 -0300

    SAVE

--- Tarefa_JS/main.js
+++ Tarefa_JS/main.js
@@ -1,12 +1,13 @@
-const form = document.getElementById('form-tarefa');
+const form = document.querySelector('form-tarefa');
 const valorA = document.getElementById('valor-a');
+const valorB = document.getElementById('valor-b');
 let formEValido = false;
 
 function valorTotal(valorA, valorB) {
     return valorA < valorB;
 }
 
-form.addEventListener(submmit, function(e){
+form.addEventListener('submmit', function(e){
     e.preventDefault();
 
 
@@ -14,16 +15,16 @@ const valorA = document.getElementById('valor-a');
 const valorB = document.getElementById('valor-b');
 const messagemSucesso = `1º numero <b>${valorA.value}</b> 2º numero <b>${valorB.value}</b>`
 
-formEValido = valorTotal(valorA.value);
+formEValido = valorTotal(valorA.value, valorB.value);
 if (formEValido) {
     const containerMensagemSucesso = document.querySelector('.success-message');
     containerMensagemSucesso.innerHTML = messagemSucesso;
-    containerMensagemSucesso.computedStyleMap.display = 'block';
+    containerMensagemSucesso.style.display = 'block';
 }else {
     valorA.style.border = '1px solid red';
     document.querySelector('error-message').style.display = 'block';
 }
-})
+});
 
 valorA.addEventListener('change', function(e) {
     console.log(e.target.value)
@@ -36,4 +37,4 @@ valorA.addEventListener('change', function(e) {
         valorA.classList.remove('error');
         document.querySelector('.error-message').style.display = 'none';
     }
-})
+});
