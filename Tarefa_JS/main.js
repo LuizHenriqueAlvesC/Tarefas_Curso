182728041620f2eeda82af3060c88424b2a48db7
luiz980alves@gmail.com <luiz980alves@gmail.com>
2024-02-12 21:53:18 -0300

    save

--- Tarefa_JS/main.js
+++ Tarefa_JS/main.js
@@ -10,9 +10,6 @@ function valorTotal(valorA, valorB) {
 form.addEventListener('submmit', function(e){
     e.preventDefault();
 
-
-const valorA = document.getElementById('valor-a');
-const valorB = document.getElementById('valor-b');
 const messagemSucesso = `1º numero <b>${valorA.value}</b> 2º numero <b>${valorB.value}</b>`
 
 formEValido = valorTotal(valorA.value, valorB.value);
@@ -20,20 +17,20 @@ if (formEValido) {
     const containerMensagemSucesso = document.querySelector('.success-message');
     containerMensagemSucesso.innerHTML = messagemSucesso;
     containerMensagemSucesso.style.display = 'block';
-}else {
+} else {
     valorA.style.border = '1px solid red';
     document.querySelector('error-message').style.display = 'block';
 }
-});
+})
 
 valorA.addEventListener('change', function(e) {
     console.log(e.target.value)
-    formEValido = valorTotal(e.target.value)
+    formEValido = valorTotal(e.target.value, valorB.value);
 
     if (!formEValido) {
         valorA.classList.add('error');
         document.querySelector('.error-message').style.display = 'block';
-    }else {
+    } else {
         valorA.classList.remove('error');
         document.querySelector('.error-message').style.display = 'none';
     }
